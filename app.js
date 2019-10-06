b8ed1459658110b3355bad650d4833034630aa3a
wangdongxu <wangdongxu@vipkid.com.cn>
2019-10-06 17:47:09 +0800

    多个中间键

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
-
-app.use((ctx) => {
-    ctx.body = "hello, yueqi111"
+const router = new Router({
+    prefix: '/users'
 });
 
-app.listen(3000);
+// 多中间键用法
+const auth = async (ctx, next) => {
+    if(ctx.url !== '/users') {
+        ctx.throw(401);
+    } 
+
+    await next();
+}
+
+router.get('/', auth, (ctx) => {
+    ctx.body = `这里是用户列表页`
+})
+
+router.get('/:id', auth, (ctx) => {
+    ctx.body = `这里是用户列表页${ctx.params.id}`
+})
+
+router.post('/', auth, (ctx) => {
+    ctx.body = "这是创建用户"
+})
+
+app.use(router.routes());
+// 所有的接口支持options请求
+app.use(router.allowedMethods());
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('server is listenint on 3000')
+});
